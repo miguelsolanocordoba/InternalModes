5f7e0df6631c3cd4e795aea08c155f1614de2fa6
Miguel Solano <miguel.solano.cordoba@gmail.com>
2020-07-10 15:35:29 -0500

    eigen_const fixed. Working version!
    Cleaned up the code and removed interactive mode. Now all plots
    are printed by default. Note Ueig normalized does not have amplitude
    of 1, but all are normalized "Maarten's way".

--- eigen_const.m
+++ eigen_const.m
@@ -1,4 +1,4 @@
-clc; clear; close all; 
+clc; clear; close all; warning off 
 %EIGEN_CONST solves the EVP for constant stratification.  
 % EIGEN_CONST is an interactive script that solved and 
 % plots the solution to the Sturm-Liouville 
@@ -35,109 +35,106 @@ n = 2*64;     % Number of modes and vertical grid points
 zf = linspace(zIn(1),zIn(2),n); % Output grid 
 dz = diff(zf); 
 zc = zf(1:end-1) + dz/2;
-%zc = zf(1:end-1)/2 + zf(2:end)/2;
 nz = numel(zc); 
 H = zIn(1); 
 
 % Compute modes using Jeffrey's toolkit
-imf = InternalModes(rhoFunc,zIn,zf,lat,'normalization','wMax'); %,...
-imc = InternalModes(rhoFunc,zIn,zc,lat,'normalization','wMax'); %,...
-       %   'nModes',n,'method','finiteDifference'); 
+imf = InternalModes(rhoFunc,zIn,zf,lat); %,...
+imc = InternalModes(rhoFunc,zIn,zc,lat); %,...
 
 f =  imf.f0;  %  Coriolis frequency 
 
 % Plot density 
-plotf = input('Plot density? Y/N [N]','s'); 
-if plotf=='Y'
-  figure
-  plot(rhoFunc(zIn),zIn); 
-  xlabel('\rho'); ylabel('Depth[m]')
-  text(1035,-1000,['N_0 = ' num2str(N0)])
-  print('const_rho.png','-r300','-dpng') 
-end
+figure
+plot(rhoFunc(zIn),zIn); 
+xlabel('\rho'); ylabel('Depth[m]')
+text(1035,-1000,['N_0 = ' num2str(N0)])
+print('const_rho.png','-r300','-dpng') 
 
-%% Maarten (sturm_liouville_hyd_normalize.m) 
-%dz = z(2) -  z(1); 
-%Nb = N0*ones(size(z));
-%[~,~,~,Weig,Ueig] = sturm_liouville_hyd_normalize(omega,Nb,dz,f);
+%% Compute Eigenfunctions
+%% Maarten (sturm_liouville_hyd_normalize.m) ; 
+[~,~,~,WeigM,UeigM] = sturm_liouville_hyd_normalize(omega,N0*ones(size(zf)),dz(1),f);
+WeigM = WeigM/(max(max(WeigM)));
 
+%% Oladeji
 N2 = N2Func(zc); 
 rho = rhoFunc(zc); 
-[~,~,~,Weig,Ueig] = compute_eigen(rho',zf',f,omega); 
-%[Weig,Ueig] = comp_eigen_ola(dz,N2,f,omega); 
-
-% Plot eigenvalues
-zc2 = zf(1:end-1) + diff(zf)/2;
-%N =  numel(zf); 
-%AA = repmat(-sum(Ueig.^2.*dz,1)./H,[N 1]).^(1/2); 
-%normU = max(max(abs(Ueig))); 
-%normW = max(max(abs(Weig))); 
-
-plotf = input('Plot Olas modes? Y/N [N]','s'); 
-if plotf=='Y'
-   figure 
-   subplot(121)
-   plot(Ueig(:,1:5),zc)
-   %plot(Ueig(:,2:6)./normU,flipud(zf'))
-   title('Horizontal Eigenvalues')
-   xlabel('U_{eig}'); ylabel('Depth [m]')
-   
-   subplot(122)
-   plot(Weig(:,1:5),zf)
-   title('Vertical Eigenvalues')
-   xlabel('W_{eig}'); ylabel('Depth [m]')
-   legend('n=1','n=2','n=3','n=4','n=5','Location','NorthEast')
-   print('const_eigen_ola.png','-r300','-dpng') 
-end
+[~,~,~,WeigO,UeigO] = compute_eigen(rho',zf',f,omega); 
 
 %% Jeffrey (InternalModes.m)
-%im.normalization = 'uMax'; 
-%[UeigJ,~,~,~] = im.ModesAtFrequency(omega); 
+%imc.normalization = 'uMax'; 
+%[UeigJ,~,~,~] = imc.ModesAtFrequency(omega); 
 
-im.normalization = 'wMax'; 
+imf.normalization = 'wMax'; 
 [~,WeigJ,~,~] = imf.ModesAtFrequency(omega); 
-UeigJ = compute_ueig(Weig,dz'); 
+UeigJ = compute_ueig(WeigJ,dz'); 
+umax = max(UeigJ(2,:)); 
 
 
-% Plot eigenvalues
-plotf = input('Plot Jeffs modes? Y/N [N]','s'); 
-if plotf=='Y'
-   figure 
-   subplot(121)
-   plot(UeigJ(:,1:5),zc)
-   title('Horizontal Eigenvalues')
-   xlabel('U_{eig}'); ylabel('Depth [m]')
-   legend('n=1','n=2','n=3','n=4','n=5','Location','NorthWest')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PLOTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Oladeji
+figure 
+subplot(121)
+plot(UeigO(:,1:5),zc)
+title('Horizontal Eigenvalues')
+xlabel('U_{eig}'); ylabel('Depth [m]'); xlim([-umax umax])
+
+subplot(122)
+plot(WeigO(:,1:5),zf)
+title('Vertical Eigenvalues')
+xlabel('W_{eig}'); ylabel('Depth [m]')
+%legend('n=1','n=2','n=3','n=4','n=5','Location','NorthEast')
+print('const_eigen_ola.png','-r300','-dpng') 
+
+
+% Maarten 
+figure 
+subplot(121)
+plot(UeigM(:,2:6),fliplr(zc))
+title('Horizontal Eigenvalues')
+xlabel('U_{eig}'); ylabel('Depth [m]'); xlim([-umax umax]) 
+%legend('n=1','n=2','n=3','n=4','n=5','Location','NorthWest')
    
-   subplot(122)
-   plot(WeigJ(:,1:5),zf)
-   title('Vertical Eigenvalues')
-   xlabel('W_{eig}'); ylabel('Depth [m]')
-   legend('n=1','n=2','n=3','n=4','n=5','Location','NorthEast')
-   print('const_eigen_jeff.png','-r300','-dpng') 
-end
+subplot(122)
+plot(-WeigM(:,2:6),fliplr(zc))
+title('Vertical Eigenvalues')
+xlabel('W_{eig}'); ylabel('Depth [m]')
+%legend('n=1','n=2','n=3','n=4','n=5','Location','NorthEast')
+print('const_eigen_maar.png','-r300','-dpng') 
 
 
-%% Plot comparison: Jeffrey vs Maarten
-plotf = input('Plot compare modes? Y/N [N]','s'); 
-if plotf=='Y'
-   figure 
-   subplot(121)
-   plot(UeigJ(:,1:5),zc); hold on
-   %plot(Ueig(:,2:6)./normU,flipud(zf'),'+k','MarkerSize',2)
-   plot(Ueig(:,1:5),zc,'+k','MarkerSize',2)
-   title('Horizontal Eigenvalues')
-   xlabel('U_{eig}'); ylabel('Depth [m]')
+% Jeffrey Early  
+figure 
+subplot(121)
+plot(UeigJ(:,1:5),zc)
+title('Horizontal Eigenvalues')
+xlabel('U_{eig}'); ylabel('Depth [m]'); xlim([-umax umax])
+%legend('n=1','n=2','n=3','n=4','n=5','Location','NorthWest')
    
-   subplot(122)
-   plot(WeigJ(:,1:5),zf); hold on
-   %plot(Weig(:,2:6)./normW,flipud(zf'),'+k','MarkerSize',2)
-   plot(Weig(:,1:5),zf,'+k','MarkerSize',2)
-   title('Vertical Eigenvalues')
-   xlabel('W_{eig}'); ylabel('Depth [m]')
-   legend('n=1','n=2','n=3','n=4','n=5','Location','NorthEast')
-   print('const_eigen_comp.png','-r300','-dpng')
-end
+subplot(122)
+plot(WeigJ(:,1:5),zf)
+title('Vertical Eigenvalues')
+xlabel('W_{eig}'); ylabel('Depth [m]')
+%legend('n=1','n=2','n=3','n=4','n=5','Location','NorthEast')
+print('const_eigen_jeff.png','-r300','-dpng') 
+
+
+% Comparison (Maar vs Jeff)
+figure 
+subplot(121)
+plot(UeigM(:,2:6),fliplr(zc)); hold on
+plot(UeigJ(:,1:5),zc,'+k','MarkerSize',2)
+title('Horizontal Eigenvalues')
+xlabel('U_{eig}'); ylabel('Depth [m]'); xlim([-umax umax])
+
+subplot(122)
+plot(-WeigM(:,2:6),fliplr(zc)); hold on
+plot(WeigJ(:,1:5),zf,'+k','MarkerSize',2)
+title('Vertical Eigenvalues')
+xlabel('W_{eig}'); ylabel('Depth [m]')
+%legend('n=1','n=2','n=3','n=4','n=5','Location','NorthEast')
+print('const_eigen_comp.png','-r300','-dpng')
+
 
 % Move all figures to /data
-system(['mv *.png ' figpath]);
+system(['mv *.png ' figpath]); 
